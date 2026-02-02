5872622dd189728ff1b635cbe35949e6e3c4824e
Gnashal <yousifceballos@gmail.com>
2026-02-02 22:31:59 +0800

    add: Growth Index

--- types/adminTypes.go
+++ types/adminTypes.go
@@ -3,21 +3,24 @@ type AdminDashboardRequest struct {
 	AdminID    string `json:"adminId"     binding:"required"`
 	DateFilter string `json:"dateFilter"  binding:"required"`
 }
-
-
-type AdminDashboardResponse struct {
+type DashboardData struct {
 	Sales int32 `json:"sales"`
 	Bookings int32 `json:"bookings"`
 	ActiveSessions int32 `json:"activeSessions"`
 	Clients int32 `json:"clients"`
 }
+type AdminDashboardResponse struct {
+	Sales int32 `json:"sales"`
+	Bookings int32 `json:"bookings"`
+	ActiveSessions int32 `json:"activeSessions"`
+	Clients int32 `json:"clients"`
+	GrowthIndex GrowthIndex `json:"growthIndex"`
+}
 
-type AdminDashboardGrowthIndex struct {
+type GrowthIndex struct {
 	SalesGrowthIndex float64 `json:"salesGrowthIndex"`
 	BookingsGrowthIndex float64 `json:"bookingsGrowthIndex"`
-	ActiveSessionsGrowthIndex float64 `json:"activeSessionsGrowthIndex"`
-	ClientGrowthIndex ClientsGrowthIndex `json:"clientsGrowthIndex"`
-	
+	ActiveSessionsGrowthIndex float64 `json:"activeSessionsGrowthIndex"`	
 }
 type ClientsGrowthIndex struct {
 	New int32 `json:"new"`
