b596844ce861d993dd415fbe4fcb88f183c57a4f
RubyDEAD <reitzdave.andriano@gmail.com>
2026-02-26 18:08:55 +0800

    asd

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 	// public paths for Clerk middleware
 	publicPaths := []string{"/api/account/customer/signup",
 		"/api/account/employee/signup", "/api/account/admin/signup",
-		"/api/payment/quote/preview", "/health", "/api/admin/dashboard", "/api/payment/quote"}
+		"/api/payment/quote/preview", "/health", "/api/admin/dashboard", "/api/payment/quote", "/api/booking/slots", "/api/booking/customer", "/api/booking/bookings"}
 
 	// websocket
 	hubs := realtime.NewRealtimeHubs(logger)
