e2a3ff154eb1f4b2b2c0cebfde365551221fa828
Gnashal <yousifceballos@gmail.com>
2026-02-27 21:09:51 +0800

    nit: follow up commits not PR;d

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -59,8 +59,12 @@ func BookingEndpoint(r *gin.RouterGroup, h *handlers.BookingHandler) {
 func PaymentEndpoint(r *gin.RouterGroup, h *handlers.PaymentHandler) {
 	r.POST("/quote", h.MakeQuotation)
 	r.POST("/quote/preview", h.MakePublicQuotation)
-	r.GET("/quotes", h.GetAllQuotesFromCustomer)
+	r.GET("/quotes", h.GetAllQuotes)
 	r.GET("/quote", h.GetQuoteByIDForCustomer)
+	customer := r.Group("/customer")
+	{
+		customer.GET("/quotes", h.GetAllQuotesFromCustomer)
+	}
 }
 
 func AdminEndpoint(r *gin.RouterGroup, h *handlers.AdminHandler) {
